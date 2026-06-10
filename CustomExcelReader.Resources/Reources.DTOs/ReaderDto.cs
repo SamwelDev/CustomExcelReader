2fa1a4bcad2053e71c8e31f1f1c25df41e5aafec
samwel.mwamakula <samwel.mwamakula@engo.co.tz>
2026-06-10 22:58:13 +0300

    BUILD & UPDATE : adding of project reference between Reources layer and UI also Dto of Excel Reader.

---
+++ CustomExcelReader.Resources/Reources.DTOs/ReaderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomExcelReader.Resources.Reources.DTOs
+{
+    public  class ReaderDto
+    {
+        public long Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
